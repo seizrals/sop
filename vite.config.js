5ce7a6c9d2e63391f85c10197b8a6bbdc2bd7fa8
seizrals <seizraaulia@gmail.com>
2026-08-27 22:21:40 +0800

    perbaikan vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/img/**/*.png',
+                'resources/img/**/*.jpg',
+                'resources/img/**/*.jpeg',
+            ],
             refresh: true,
         }),
     ],
