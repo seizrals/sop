a03b0c3a99aa0e9d713e98b6f3810981bf94b0e2
seizrals <seizraaulia@gmail.com>
2026-08-27 21:46:19 +0800

    otomatis ke halaman login

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\UserManagementController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return auth()->check() ? redirect()->route('dashboard') : redirect()->route('login');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
